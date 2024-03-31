dbc111889cf57e58e8b840c8fb8671e0ff64eda0
Eduardo Neves <e@observability.works>
2024-03-31 14:56:20 -0300

    First Version to generate http log

--- fakelog/generator.py
+++ fakelog/generator.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python
-import time
 import datetime
-import pytz
-import numpy
+import time
 import random
-import gzip
-import zipfile
 import sys
 import argparse
 from faker import Faker
 from faker.providers import internet
-from random import randrange
-#from tzlocal import get_localzone
 
+log_lines = 1000
 fake = Faker()
-#otime = datetime.datetime.now()
 tz = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
-#fake.add_provider(internet)
 status_code = ['200','301','302','400','401','403','404','500']
-for _ in range(100):
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+filename = 'access_log_'+timestr+'.log'
+f = open(filename,'w')
+for _ in range(log_lines):
     ip = fake.ipv4()
     otime = fake.date_time_between('-1M')
     dt = otime.strftime('%d/%b/%Y:%H:%M:%S')
@@ -28,5 +25,6 @@ for _ in range(100):
     byte = int(random.gauss(5000,50))
     ref = fake.uri()
     user_agent = fake.user_agent()
-    #,vrb,uri,resp,byt
-    print(f'%s - - [%s %s00] "%s /%s HTTP/1.0" %s %s "%s" "%s"' % (ip,dt,tz,vrb,uri,resp,byte,ref,user_agent))
+    f.write('%s - - [%s %s00] "%s /%s HTTP/1.0" %s %s "%s" "%s"\n' % (ip,dt,tz,vrb,uri,resp,byte,ref,user_agent))
+    f.flush()
+f.close()
